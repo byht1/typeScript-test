8a7adf11902b38279324379aba40fc723d162d58
byht1 <vitalikbyht1@gmail.com>
2022-07-28 16:59:28 +0300

    Update Calc.tsx

--- src/components/Main/Calc/Calc.tsx
+++ src/components/Main/Calc/Calc.tsx
@@ -31,39 +31,54 @@ export const Calc: FC = () => {
   const [currencyTo, setCurrencyTo] = useState<string>('UAH');
   const [currencyResult, setCurrencyResult] = useState<string>('USD');
   const [course, setСourse] = useState<number>(0);
-  // const { register, handleSubmit } = useForm<FormData>();
-  let v = 1;
+
   useEffect(() => {
     dataServer(valueCalcOne);
   }, []);
 
-  // useEffect(() => {
-  //   dataServer(valueCalcOne);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [valueCalcOne, currencyTo]);
+  async function dataServer(
+    value: string,
+    n: number = 1,
+    to: string = 'UAH',
+    result: string = 'USD'
+  ) {
+    const data = await cacl(value, to, result);
+    const course = Number((1 / data.result).toFixed(2));
 
-  useEffect(() => {
-    dataServer(valueCalcTwo, 2);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [valueCalcTwo, currencyResult]);
-
-  // const sublitHendler = handleSubmit(({ value, currencyOne, currencyTwo }) => {
-  //   // dataServer();
-  // });
-
-  async function dataServer(value: string, n: number = 1) {
-    const data = await cacl(value, currencyTo, currencyResult);
-
-    setСourse(data.result);
+    setСourse(course);
     if (n === 1) {
       const total: number = (await data.result) * Number(value);
       setValueCalcTwo(total.toFixed(2));
-    } else {
-      const total: number = await (Number(value) / data.result);
-      console.log('🚀 ~ total', total);
-      setValueCalcOne(total.toFixed(2));
+      return;
     }
+    const total: number = await (Number(value) / data.result);
+    setValueCalcOne(total.toFixed(2));
   }
+
+  const onChangeValueCalcOne = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue: string = event.target.value;
+    setValueCalcOne(newValue);
+    dataServer(newValue);
+  };
+
+  const onChangeValueCalcTwo = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue: string = event.target.value;
+    setValueCalcTwo(newValue);
+    dataServer(newValue, 2);
+  };
+
+  const onChangeCurrencyTo = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue: string = event.target.value;
+    setCurrencyTo(newValue);
+    dataServer(valueCalcOne, 1, newValue, currencyResult);
+  };
+  const onChangeCurrencyResult = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const newValue: string = event.target.value;
+    setCurrencyResult(newValue);
+    dataServer(valueCalcOne, 2, currencyTo, newValue);
+  };
 
   return (
     <CalcFormBox>
@@ -72,15 +87,13 @@ export const Calc: FC = () => {
           <input
             type="number"
             value={valueCalcOne}
-            onChange={event => setValueCalcOne(event.target.value)}
+            onChange={onChangeValueCalcOne}
           />
-          <select onChange={event => setCurrencyTo(event.target.value)}>
+          <select
+            onChange={event => onChangeCurrencyTo(event)}
+            value={currencyTo}
+          >
             {valueName.map(x => {
-              if (x === 'UAH') {
-                <option selected key={x + '1'} value={x}>
-                  {x}
-                </option>;
-              }
               return (
                 <option key={x + '1'} value={x}>
                   {x}
@@ -93,17 +106,13 @@ export const Calc: FC = () => {
           <input
             value={valueCalcTwo}
             type="number"
-            onChange={event => setValueCalcTwo(event.target.value)}
+            onChange={onChangeValueCalcTwo}
           />
-          <select onChange={event => setCurrencyResult(event.target.value)}>
+          <select
+            onChange={event => onChangeCurrencyResult(event)}
+            value={currencyResult}
+          >
             {valueName.map(x => {
-              if (x === 'USD') {
-                return (
-                  <option selected key={x + '2'} value={x}>
-                    {x}
-                  </option>
-                );
-              }
               return (
                 <option key={x + '2'} value={x}>
                   {x}
